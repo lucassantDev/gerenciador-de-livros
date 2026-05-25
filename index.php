f8120db064ca263f1de40a427f1d1285e0d235f5
lucassant <lucasfdesantana095@gmail.com>
2026-05-25 11:46:20 -0300

    refactor: chamando os dados por require

--- index.php
+++ index.php
@@ -2,65 +2,8 @@
 
 require 'dados.php';
   
+$view = 'index';
+
+require 'views/template/app.php';
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
-    <title>Gerenciador</title>
-</head>
-<body class="bg-stone-950 text-stone-200">
-
-    <header class="bg-stone-800 border-b-stone-800 ">
-        <nav class="mx-auto max-w-screen-lg flex justify-between px-8 py-4" >
-            <div class="font-bold text-xl trackin-wide">BOOK WISE</div>
-            <ul class="flex space-x-4 font-bold">
-                <li><a href="/" class="text-orange-400">Explorar</a></li>
-                <li><a href="/mues-livros.php" class="hover:underline">Meus Livros</a></li>
-            </ul>
-            <ul>
-                <li><a href="/login.php">Fazer Login</a></li>
-            </ul>
-        </nav>
-    </header>
-
-    <main class="mx-auto max-w-screen-lg space-y-6">
-        <form class="w-full flex space-x-2 mt-6">
-            <input 
-                type="text"
-                class="border-stone-800 border-2 rounded-md bg-stone-900 text-sm focus:outline-none px-2 py-1"
-                placeholder="Pesquisar..."
-            >
-            <button type="submit">🔍</button>
-        </form>
-
-        <!-- lista de livros -->
-        <section class="grid gap-4 grid-cols-1 md:grid grid-cols-2 lg:grid grid-cols-3">
-            <!-- Livro -->
-
-            <?php foreach($livros as $livro): ?>
-
-                <div class="p-2 rounded border-stone-800 border-2 bg-stone-900">
-                    <div class="flex">
-                        <div class="w-1/3">Imagem</div>
-                        <div class="space-y-2">
-                            <a href="/livro.php?id=<?=$livro['id']?>" class="fon-semibold hover:underline"><?=$livro['titulo']?></a>
-                            <div class="text-xs italic"><?=$livro['Autor']?></div>
-                            <div class="text-xs italic">⭐⭐⭐⭐⭐(3 Avaliação)</div>
-                        </div>
-                    </div>
-                    <div class="text-sm mt-2">
-                        <?=$livro['Descricao'] ?>
-                    </div>
-                </div>
-
-            <?php endforeach; ?>
-
-        </section>
-    </main>
-
-</body>
-</html>
--- livro.php
+++ livro.php
@@ -6,52 +6,6 @@
     $livro = array_pop($filtrado);
 
 
+    $view = 'livro';
+    require 'views/template/app.php';
 ?>
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
-    <title>Gerenciador</title>
-</head>
-<body class="bg-stone-950 text-stone-200">
-
-    <header class="bg-stone-800 border-b-stone-800 ">
-        <nav class="mx-auto max-w-screen-lg flex justify-between px-8 py-4" >
-            <div class="font-bold text-xl trackin-wide">BOOK WISE</div>
-            <ul class="flex space-x-4 font-bold">
-                <li><a href="/" class="text-orange-400">Explorar</a></li>
-                <li><a href="/mues-livros.php" class="hover:underline">Meus Livros</a></li>
-            </ul>
-            <ul>
-                <li><a href="/login.php">Fazer Login</a></li>
-            </ul>
-        </nav>
-    </header>
-
-    <main class="mx-auto max-w-screen-lg space-y-6">
-
-        <!-- mudando aqui -->
-
-        <?=$livro['titulo'] ?>
-        <div class="p-2 rounded border-stone-800 border-2 bg-stone-900">
-            <div class="flex">
-                <div class="w-1/3">Imagem</div>
-                <div class="space-y-2">
-                    <a href="/livro.php?id=<?=$livro['id']?>" class="fon-semibold hover:underline"><?=$livro['titulo']?></a>
-                    <div class="text-xs italic"><?=$livro['Autor']?></div>
-                    <div class="text-xs italic">⭐⭐⭐⭐⭐(3 Avaliação)</div>
-                </div>
-            </div>
-            <div class="text-sm mt-2">
-                <?=$livro['Descricao'] ?>
-            </div>
-        </div>
-
-    </main>
-
-</body>
-</html>
